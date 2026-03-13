07a2bae7d9044f1c9ac2d005a7e936dfacfb902e
TediBalint <tedi.balint@students.jedlik.eu>
2026-03-14 00:06:12 +0100

    Added AuthEntry View, Implemented the login UI on the MainPage.

--- kulonut-Mobil/ViewModels/MainViewModel.cs
+++ kulonut-Mobil/ViewModels/MainViewModel.cs
@@ -1,20 +1,18 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
 using kulonut_Mobil.API;
-using kulonut_Mobil.Models;
-using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace kulonut_Mobil.ViewModels
 {
 	public partial class MainViewModel : ObservableObject
 	{
 		private IApiClient apiClient;
+
+		[ObservableProperty]
+		private string bindTest;
 		public MainViewModel(IApiClient _apiClient)
 		{
+			apiClient = _apiClient;
+			BindTest = "BindTest";
 		}
 	}
 }
