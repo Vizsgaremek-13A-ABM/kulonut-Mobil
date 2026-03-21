86ccea9e73e3f8cf2019b0071c5db0aa2dd63bee
TediBalint <tedi.balint@students.jedlik.eu>
2026-03-21 20:23:07 +0100

    Made Basic Navigation for PasswordResetPage

--- kulonut-Mobil/ViewModels/PasswordResetViewModel.cs
+++ kulonut-Mobil/ViewModels/PasswordResetViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using kulonut_Mobil.Pages;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,8 +7,21 @@ using System.Threading.Tasks;
 
 namespace kulonut_Mobil.ViewModels
 {
+	[QueryProperty(nameof(NavigatedFrom), NAV_URL)]
 	public partial class PasswordResetViewModel : BaseViewModel
 	{
-
+		public const string NAV_URL = "navigatedFrom";
+		public string? NavigatedFrom { get; set; }
+		public override bool OnBackButtonPressed()
+		{
+			MainThread.BeginInvokeOnMainThread(async () =>
+			{
+				if(NavigatedFrom==nameof(PasswordChangePage))
+					await Shell.Current.GoToAsync($"{NavigatedFrom}");
+				else
+					await Shell.Current.GoToAsync($"//{NavigatedFrom}");
+			});
+			return true;
+		}
 	}
 }
