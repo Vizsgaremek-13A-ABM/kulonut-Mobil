fb9fe0681c0d1198eda64d4ef962596968aeaee4
TediBalint <tedi.balint@students.jedlik.eu>
2026-03-21 12:04:08 +0100

    Created Main Models

---
+++ kulonut-Mobil/Models/UserModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kulonut_Mobil.Models
+{
+	public class UserModel
+	{		
+		public int id { get; set; }
+		public string name { get; set; }
+		public string display_name { get; set; }
+		public string email { get; set; }
+		public string avatar { get; set; }
+		public RoleModel? role { get; set; }
+		public DateTime joined_at { get; set; }
+	}
+}
