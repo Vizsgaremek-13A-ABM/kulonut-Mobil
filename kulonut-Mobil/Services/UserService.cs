d51e517a006304c8d2fe25f019e72ce5f5cf1830
TediBalint <tedi.balint@students.jedlik.eu>
2026-03-22 00:24:16 +0100

    Implemented basic UserService functionalities

---
+++ kulonut-Mobil/Services/UserService.cs
@@ -0,0 +1,44 @@
+﻿using kulonut_Mobil.API;
+using kulonut_Mobil.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kulonut_Mobil.Services
+{
+    public class UserService : IUserService
+    {
+        private UserModel? user;
+		private IApiClient apiClient;
+		public UserService(IApiClient _apiClient)
+		{
+			apiClient = _apiClient;
+		}
+
+		public UserModel? GetCurrentUser()
+		{
+			return user;
+		}
+
+		public async Task<UserModel?> GetCurrentUserAsync()
+		{
+			return await apiClient.GetWithCachingAsync<UserModel?>("/users", "user");
+		}
+
+		public void SetCurrentUser(UserModel _user)
+		{
+			user = _user;
+ 		}
+
+		public Task<UserModel> UpdateCurrentUserAsync(UserModel? request)
+		{
+			throw new NotImplementedException();
+		}
+		public void ClearCurrentUser()
+		{
+			user = null;
+		}
+	}
+}
