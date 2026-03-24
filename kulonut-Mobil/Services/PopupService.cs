65145d0f690049d7223805f9f08a3238ad32fc70
TediBalint <tedi.balint@students.jedlik.eu>
2026-03-24 10:07:31 +0100

    Added basic Input check

---
+++ kulonut-Mobil/Services/PopupService.cs
@@ -0,0 +1,17 @@
+﻿using Android.Provider;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kulonut_Mobil.Services
+{
+    public class PopupService : IPopupService
+    {
+        public async Task ShowErrorAsync(string message)
+        {
+            await Application.Current!.MainPage!.DisplayAlert("Error", message, "Ok");
+        }
+    }
+}
--- kulonut-Mobil/ViewModels/MainViewModel.cs
+++ kulonut-Mobil/ViewModels/MainViewModel.cs
@@ -1,36 +1,67 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
 using kulonut_Mobil.API;
+using kulonut_Mobil.Models;
+using kulonut_Mobil.Models.DTOs;
 using kulonut_Mobil.Pages;
+using kulonut_Mobil.Services;
+using kulonut_Mobil.Validation;
 using System.Threading.Tasks;
 
 namespace kulonut_Mobil.ViewModels
 {
 	public partial class MainViewModel : BaseViewModel
 	{
-		private IApiClient apiClient;
 
 		[ObservableProperty]
-		private string bindTest;
-		public MainViewModel(IApiClient _apiClient)
+		private LoginRequestDTO loginRequestDTO = new LoginRequestDTO();
+
+		[ObservableProperty]
+		private bool rememberLogin = false;
+
+		private IAuthService authService;
+		private IPopupService popupService;
+
+		public MainViewModel(IAuthService _authService, IPopupService _popupService)
 		{
-			apiClient = _apiClient;
-			BindTest = "BindTest";
+			authService = _authService;
+			popupService = _popupService;
 		}
+		
 		[RelayCommand]
-		private async Task NavigateToMap()
+		private async Task Login()
 		{
+			bool check_result = await InputCheck();
+			if (!check_result) return;
+			UserModel? response = await authService.LoginAsync(LoginRequestDTO, RememberLogin);
+			if(response == null)
 			await Shell.Current.GoToAsync($"//{nameof(MapPage)}");
 		}
 		[RelayCommand]
-		private async Task NavigateToRegister()
+		private async Task Register()
 		{
 			await Shell.Current.GoToAsync($"{nameof(RegisterPage)}");
 		}
 		[RelayCommand]
-		private async Task NavigateToPasswordResetPage()
+		private async Task PasswordReset()
 		{
 			await Shell.Current.GoToAsync($"{nameof(PasswordResetPage)}?{PasswordResetViewModel.NAV_URL}={nameof(MainPage)}");
+		}
+		private async Task<bool> InputCheck()
+		{
+			string? email_valid = InputValidator.ValidateEmail(LoginRequestDTO.Email);
+            if(email_valid != null)
+			{
+				await popupService.ShowErrorAsync(email_valid);
+				return false;
+			}
+            string? user_password = InputValidator.ValidateName(LoginRequestDTO.Password);
+            if (user_password != null)
+            {
+                await popupService.ShowErrorAsync(user_password);
+                return false;
+            }
+            return true;
 		}
 	}
 }
